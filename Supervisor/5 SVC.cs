729660c5e16b8ca16aa4a340399c18af3581ca55
BrakUszanowanka <goszczynskip@gmail.com>
2014-01-19 16:04:17 +0100

    Szkielet

--- Supervisor/5 SVC.cs
+++ Supervisor/5 SVC.cs
@@ -4,12 +4,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Processor;
+using Process;
 using Memory;
+using External;
 
 namespace Supervisor
 {
     class SVC
     {
+        //Na samym początku Tworzy procesy *IN i *OUT
+        //Poprzez Komunikację z Procesem *IN szuka karty (linijki) $JOB
 
 
 
@@ -23,7 +27,8 @@ namespace Supervisor
        static void Main()
        {
            Mem.start();//całą pamięć wolną opisuje przy pomocy bloków FSB
-
+           //Tworzy procesy SVC dla każdego strumienia zleceń (2x)
+           Proc.XC("*IPSUB");
        }
    }
 }
